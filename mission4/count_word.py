4db2d1bd2c39a81f38b9d8e4b69d57ab034bf70c
MACLSH <gracia10@daum.net>
2022-09-07 16:42:11 +0900

    Fix count 적용 및 for문 제거

--- mission4/count_word.py
+++ mission4/count_word.py
@@ -1,23 +1,14 @@
-origin_text = """안녕하세요.
+ORIGIN_TEXT = """안녕하세요.파이썬
 반갑습니다. 파이썬 공부는 정말 재밌습 니  다.
 """
 
-# 조회 단어 갯수 출력 후 파일 저장
-def count_word(text, serach):
-    counter = 0
-    for word in text.split():
-        print(word)
-        if word.find(serach) >= 0:
-            counter += 1
 
-    print(counter)
-    save(text)
+def count_word(origin_text, search):
+    """조회 단어 갯수 출력 후 파일 저장"""
+    print(origin_text.count(search))
+
+    with open("count_word.txt", "w", encoding="utf-8") as fhand:
+        fhand.write(origin_text)
 
 
-# 파일 생성 or 조회 후 핸들러 종료
-def save(text):
-    with open("count_word.txt", "w") as fhand:
-        fhand.write(text)
-
-
-count_word(origin_text, "파이썬")
+count_word(ORIGIN_TEXT, "파이썬")
