945ba59d53b5d2396405c4c65f8f7de1baed04e9
MACLSH <gracia10@daum.net>
2022-09-08 22:44:41 +0900

    Fix 디데이 카운트 상수타입 변경
    - 값의 변화가 없으므로 튜플타입으로 변경
    - 이해가 모호한 변수 제거 (results)
    - while 문 내 if 문 로직 수정

--- mission5/after_100.py
+++ mission5/after_100.py
@@ -1,46 +1,34 @@
 def after_100(month, date, day):
-    """월 , 일 , 요일을 입력하면 100일 뒤 월 , 일 ,요일 정보를 리턴하는 함수"""
+    """월,일,요일을 입력하면 디데이 뒤 월 , 일 ,요일 정보를 리턴하는 함수"""
 
-    results = [month, date, None]
-    d_day = 100
-    counter = d_day - 1
-    days = ("월", "화", "수", "목", "금", "토", "일")
-    calendar = {
-        1: 31,
-        2: 28,
-        3: 31,
-        4: 30,
-        5: 31,
-        6: 30,
-        7: 31,
-        8: 31,
-        9: 30,
-        10: 31,
-        11: 30,
-        12: 31,
-    }
+    d_day = 200
+    weeokdays = ("월", "화", "수", "목", "금", "토", "일")
+    calendar = (31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
+    origin = (month, date, day)
 
-    # 디데이 잔여일이 월별 막일까지 소요일보다 큰 경우 다음달 1일로 넘긴다  ex) 잔여일 100일 > 6월 막일까지 소요일 9일 -> 7월 1일로 변경
+    month -= 1  # 인덱스 활용을 위해 0~11월로 논리상 약속
+    left_day = d_day - 1  # 오늘부터 디데이 카운트 되므로 1제거
+
+    # 잔여일이 매월 막일까지 소요일보다 큰 경우 다음달 1일로 넘긴다  ex) 잔여일 100일 > 6월 막일까지 소요일 9일 -> 7월 1일로 변경
     while True:
-        # 현재일 기준 막일까지 소요되는 일자 확인
-        remove_cnt = calendar[results[0]] - results[1]
+
+        # 막일까지 소요되는 일자 확인
+        remove_cnt = calendar[month] - date
 
         # 디데이 잔여일이 소요일이 보다 작으면 while문 종료. 큰 경우 다음달 1일로 변경
-        if counter - remove_cnt <= 0:
+        if left_day - remove_cnt > 0:
+            left_day -= remove_cnt + 1
+            month = (month + 1) % 12
+            date = 1
+        else:  # 디데이 잔여일이 막일까지 소요일 적은경우  ex) 잔여일 3일 > 9월 막일가지 소요일 31일
+            date += left_day
             break
-        else:
-            counter -= remove_cnt + 1
-            results[0] += 1
-            results[1] = 1
-
-    # 디데이 잔여일이 막일까지 소요일 적은경우  ex) 잔여일 3일 > 9월 막일가지 소요일 31일
-    results[1] += counter
-    results[2] = days[(days.index(day) + (date + d_day) % 7)]
 
     print(
-        f"{month}월 {date}일 {day}요일부터 {d_day}일 뒤는 {results[0]}월 {results[1]}일 {results[2]}요일"
+        f"{origin[0]}월 {origin[1]}일 {origin[2]}요일부터 {d_day}일 뒤는 "
+        f"{month+1}월 {date}일 {weeokdays[((weeokdays.index(day) + d_day - 1) % 7)]}요일"
     )
 
 
-after_100(6, 21, "월")
-# 6월 21일 월요일부터 100일 뒤는 9월 28일 화요일
+after_100(6, 21, "화")
+# 6월 21일 화요일부터 100일 뒤는 9월 28일 수요일
